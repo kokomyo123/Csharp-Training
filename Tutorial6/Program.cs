5a4db89b93dc3524435ecd023a34d32ddcd7fe65
P-000129\MyoMinHtay <scm.myominhtay@gmail.com>
2022-03-07 12:37:02 +0630

    [feature/Tutorial6] Finished 100%

---
+++ Tutorial6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial6
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Tutorial6 DB");
+            Console.ReadLine();
+        }
+    }
+}
