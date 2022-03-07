035e7cabf9467353fbd60af1f0ef7afb6f895f72
P-000129\MyoMinHtay <scm.myominhtay@gmail.com>
2022-03-07 12:25:49 +0630

    [feature/Tutorial3] Finished 100%

---
+++ Tutorial3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial3
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Main
+        /// calculating two numbers
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            int first, second, result;
+            string opt;
+            Console.Write("Pls write Console Calculator in C# as like below:\n");
+            Console.Write("---------------------------------------------------------");
+            Console.Write("\n\n");
+            Console.WriteLine("Type a number,then press Enter");
+            first = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+            Console.WriteLine("Type another number,then press Enter ");
+            second = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("a-Add\ns-Subtract\nm-Multiply\nd-Divide");
+            Console.Write("Your option?");
+            opt = Console.ReadLine();
+            switch (opt)
+            {
+                case "a":
+                    result = first + second;
+                    Console.WriteLine("Your result:{0} + {1} = {2}", first, second, result);
+                    break;
+
+                case "s":
+                    result = first - second;
+                    Console.WriteLine("Your result:{0} - {1} = {2}", first, second, result);
+                    break;
+
+                case "m":
+                    result = first * second;
+                    Console.WriteLine("Your result:{0} x {1} = {2}", first, second, result);
+                    break;
+
+                case "d":
+                    result = first / second;
+                    Console.WriteLine("Your result:{0} ÷ {1} = {2}", first, second, result);
+                    break;
+
+                default:
+                    Console.WriteLine("Please choose only from opiton");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
