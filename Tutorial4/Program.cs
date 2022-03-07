ad46578e5f971b548c881fbb8596040127f32a5a
P-000129\MyoMinHtay <scm.myominhtay@gmail.com>
2022-03-07 12:30:06 +0630

    [feature/Tutorial4] Finished 100%

---
+++ Tutorial4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Tutorial4");
+            Console.ReadLine();
+        }
+    }
+}
