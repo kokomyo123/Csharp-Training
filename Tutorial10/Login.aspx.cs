254a0cbda46d5050e4c94d09e74e799fec4a2849
P-000129\MyoMinHtay <scm.myominhtay@gmail.com>
2022-03-07 13:27:55 +0630

    [feature/Tutorial10] Finished 100%

---
+++ Tutorial10/Login.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tutorial10
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        #region Variable Declaration
+
+        private SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["userDB"].ConnectionString);
+        private SqlCommand cmd;
+
+        #endregion Variable Declaration
+
+        #region Login and Reset Password
+
+        /// <summary>
+        /// Login
+        /// </summary>
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["userDB"].ConnectionString);
+            cmd = new SqlCommand("SELECT Password FROM tbl_User WHERE Name=@Name and Password=@Password", con);
+            cmd.Parameters.AddWithValue("Name", txtUserName.Text);
+            cmd.Parameters.AddWithValue("Password", txtPassword.Text);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            if (dt.Rows.Count > 0)
+            {
+                Session["name"] = txtUserName.Text;
+                Response.Redirect("Default.aspx");
+            }
+            else
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["userDB"].ConnectionString);
+                string name = txtUserName.Text;
+                int id;
+                cmd = new SqlCommand("SELECT * FROM tbl_User WHERE Name='" + name + "'", con);
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    id = Convert.ToInt32(dr["id"]);
+                    name = dr["Name"].ToString();
+                    ResetPassword(id, name);
+                }
+                con.Close();
+            }
+        }
+
+        /// <summary>
+        /// Resetting password
+        /// </summary>
+        private void ResetPassword(int id, string name)
+        {
+            Session["Email"] = name;
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["userDB"].ConnectionString);
+            cmd = new SqlCommand("Update tbl_User set Password=NULL,Status=1 WHERE id=@id", con);
+            cmd.Parameters.AddWithValue("id", id);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Response.Redirect("Reset.aspx");
+        }
+
+        #endregion Login and Reset Password
+    }
+}
---
+++ Tutorial10/Reset.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Tutorial10
+{
+    public partial class Reset : System.Web.UI.Page
+    {
+        #region Variable Declaration and Page Load
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        private string constring = ConfigurationManager.ConnectionStrings["userDB"].ConnectionString;
+
+        #endregion Variable Declaration and Page Load
+
+        #region Change new Password and Send Email
+
+        /// <summary>
+        /// Changing new password
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtNewPass.Text) && !string.IsNullOrEmpty(txtConfirmPass.Text))
+            {
+                if (txtNewPass.Text == txtConfirmPass.Text)
+                {
+                    SendEmail();
+                    string email = Session["Email"].ToString();
+                    SqlConnection con = new SqlConnection(constring);
+                    SqlCommand cmd = new SqlCommand("Update tbl_User Set Password=@Password,Status=0 Where Name=@Name", con);
+                    cmd.Parameters.AddWithValue("Name", email);
+                    cmd.Parameters.AddWithValue("Password", txtNewPass.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    Response.Redirect("Login.aspx");
+                }
+                else
+                {
+                    LtlMessage.Text = "confirm password must be same with new password";
+                }
+            }
+            else
+            {
+                LtlMessage.Text = "please fill your password and confirm password";
+            }
+        }
+
+        /// <summary>
+        /// Sending Email
+        /// </summary>
+        private void SendEmail()
+        {
+            try
+            {
+                string to = Session["Email"].ToString(); //To address
+                string from = "yehtetaung791998@gmail.com"; //From address
+                MailMessage message = new MailMessage(from, to);
+                string mailbody = "<center><h1>Resetting Password</h1></center>";
+                mailbody += "<p>Dear Sir,</p><br>";
+                mailbody += "<p style='padding-left:30px;'>Congratulation</p>";
+                mailbody += "<p style='padding-left:30px;'>Your resetting password is successful</p>";
+                mailbody += "<p style='padding-left:100px;'>From Company</p>";
+                message.Subject = "Resetting password";
+                message.Body = mailbody;
+                message.BodyEncoding = Encoding.UTF8;
+                message.IsBodyHtml = true;
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 578); //Gmail smtp
+                System.Net.NetworkCredential basicCredential1 = new
+                System.Net.NetworkCredential("yehtetaung791998@gmail.com", "password");
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = basicCredential1;
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        #endregion Change new Password and Send Email
+    }
+}
