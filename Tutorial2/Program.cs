196b789c55a1c5de40cc9274de7ded3f3211b5e4
P-000129\MyoMinHtay <scm.myominhtay@gmail.com>
2022-03-07 12:04:41 +0630

    [feature/Tutorial2] Finished 100%

---
+++ Tutorial2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial2
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Main
+        /// checking leap year and calculating century
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            int year;
+            Console.Write("Input year : ");
+            year = Convert.ToInt32(Console.ReadLine());
+            GetCenturyAndCheckLeapYear(year);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Get Century And Check LeapYear
+        /// </summary>
+        /// <param name="year"></param>
+        public static void GetCenturyAndCheckLeapYear(int year)
+        {
+            int century;
+            if (year % 400 == 0 || year >= 1000 && year % 4 == 0 && year % 100 != 0)
+            {
+                century = (year / 100) + 1;
+                Console.WriteLine(century + "," + 1);
+            }
+            else
+            {
+                century = (year / 100) + 1;
+                Console.WriteLine(century + "," + (-1));
+            }
+        }
+    }
+}
